b5005df3677b8da25e7c6e2465180cdfa8677bce
Hightinstance <1060194771@qq.com>
2020-06-10 22:38:52 +0800

    new command

--- design_methon/Command.cpp
+++ design_methon/Command.cpp
@@ -1,13 +1,16 @@
-#include<iostream>
+#include<iosrteam>
 #include<vector>
 #include<string>
 using namespace std;
 
-class Command{
-public:
-    virtual void execute() = 0;
+class Command
+{
+	public:
+	   virtual void execute() = 0;
 }
 
+
+/* 命令本身和具体实现依赖关系抽象出来，定义具体的命令实现变化 */
 class ConcreteCommand1 : public Command
 {
     string arg;
@@ -29,7 +32,7 @@ public:
         cout<< "#2 process..."<<arg<<endl;
     }
 };
-      
+        
         
 class MacroCommand : public Command
 {
@@ -43,25 +46,17 @@ public:
             c->execute();
         }
     }
-};
-
-class MacroCommand : public Command
+};        
+int main()
 {
-	vector<Command> commands;
-	void  addCommand( Command *c )
-	{
-		commands.push_back(c);
-	}
-	void  execute() override
-	{
-		 for( auto &c : commands )
-		 {
-			 c->execute;
-		 }
-	}
+
+    ConcreteCommand1 command1(receiver, "Arg ###");
+    ConcreteCommand2 command2(receiver, "Arg $$$");
+    
+    MacroCommand macro;
+    macro.addCommand(&command1);
+    macro.addCommand(&command2);
+    
+    macro.execute();
+
 }
-
-
-
-
-
